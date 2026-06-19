bde7138a58bcf71c35cd5303a8911d533b2fc627
Rishi Patel <122262469+rishipatel-1@users.noreply.github.com>
2026-06-19 20:07:22 +0530

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+class Solution:
+
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+        if not grid:
+            return
+
+        rows, cols = len(grid), len(grid[0])
+
+        INF = 2147483647
+
+        queue = deque()
+
+        directions = [[1, 0], [-1, 0],[0, 1],[0, -1]]
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    queue.append((r, c))
+
+        while queue:
+            r, c = queue.popleft()
+
+            for rowMove, colMove in directions:
+                newRow = r + rowMove
+                newCol = c + colMove
+
+                if 0 <= newRow < rows and 0 <= newCol < cols and grid[newRow][newCol] == INF:
+                    grid[newRow][newCol] = grid[r][c] + 1
+                    queue.append((newRow, newCol))

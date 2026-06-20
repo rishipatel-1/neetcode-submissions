8d568a838a53646abc1f00298dcde4a73d695134
Rishi Patel <122262469+rishipatel-1@users.noreply.github.com>
2026-06-20 22:42:46 +0530

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+
+        queue = deque()
+
+        queue.append(beginWord)
+
+        visited = set()
+
+        visited.add(beginWord)
+
+        steps = 1
+
+        def oneWordDifference(word1, word2):
+
+            difference = 0
+
+            for i in range(len(word1)):
+                if word1[i] != word2[i]:
+                    difference += 1
+
+            return difference == 1 
+
+        while queue:
+
+            queueSize = len(queue)
+
+            for _ in range(queueSize):
+
+                currentWord = queue.popleft()
+
+                if currentWord == endWord:
+                    return steps
+
+                for nextWord in wordList:
+
+                    if nextWord not in visited and oneWordDifference(currentWord, nextWord):
+                        visited.add(nextWord)
+                        queue.append(nextWord)        
+
+            steps +=1
+
+        return 0    
+
+
+        

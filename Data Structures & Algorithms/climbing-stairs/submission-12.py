9312f481ee9fd5397dd6056a46994bb58bd72ff8
Rishi Patel <122262469+rishipatel-1@users.noreply.github.com>
2026-06-22 12:21:00 +0530

    Add: climbing-stairs - submission-12

---
+++ Data Structures & Algorithms/climbing-stairs/submission-12.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    # dp[i] depends only on:
+    #
+    # dp[i-1]
+    # dp[i-2]
+    #
+    # So store only last two answers.
+    #
+    # Time : O(n)
+    # Space: O(1)
+
+    def climbStairs(self, n: int) -> int:
+        prev1 = 1 #dp[i-1]
+        prev2 = 1 #dp[i-2]
+
+        for i in range(n-1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+
+        return prev1    
+

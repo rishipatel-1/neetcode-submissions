10b9aa23e815ae8b948a9f2992f127cad4ca75fd
Rishi Patel <122262469+rishipatel-1@users.noreply.github.com>
2026-06-29 12:26:32 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+
+        dp = [[0] * (len(coins) + 1) for _ in range(amount + 1)]
+        dp[0] = [1] * (len(coins) + 1)
+
+        for a in range(1, amount + 1):
+
+            # Process coins from right to left
+            for i in range(len(coins) - 1, -1, -1):
+
+                # Skip current coin
+                dp[a][i] = dp[a][i + 1]
+
+                # Take current coin (stay on same coin)
+                if a - coins[i] >= 0:
+                    dp[a][i] += dp[a - coins[i]][i]
+
+        return dp[amount][0]

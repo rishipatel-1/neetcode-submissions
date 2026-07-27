7ebd7cd4644e2797188ae0b8285d2494f82208f6
Rishi Patel <122262469+rishipatel-1@users.noreply.github.com>
2026-07-27 23:13:49 +0530

    Add: palindrome-linked-list - submission-0

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        slow = head 
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev = None
+        curr = slow
+
+        while curr:
+
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        left = head
+        right = prev
+
+
+        while right:
+            if left.val != right.val:
+                return False
+
+            left = left.next
+            right = right.next   
+
+        return True         
+
